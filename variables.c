b9528ac1c14ef4aa614f1a5867c46e33dba84ef5
javier73 <ingenierodesoftware73@gmail.com>
2021-06-15 09:39:11 -0500

    it was create file variables.c

---
+++ variables.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+* Variables
+* Definition and more
+*/
+int main(){
+  char C = 'c'; //tamaño de un byte 0... 255
+  int a = 20; //2 bytes 
+  short e = -1; //2 bytes
+  unsigned int u = 25; //2 bytes
+  long l = 5977; //4vbytes
+  float f = 72.800; //float
+  double d = 1222.88998; //8bytes
+  printf("%.2lf", d);
+  return 0;
+}
