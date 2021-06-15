789b8b1f5f1e7a73a8724f4c98d87ef6f68d6ba9
javier73 <ingenierodesoftware73@gmail.com>
2021-06-15 09:26:11 -0500

    it was create file directives.c

---
+++ directives.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// #include "namefile"
+#define PI 3.14
+#define CUBO(a) a*a*a
+int main(){
+  // int suma;
+  // suma = PI + 3;
+  int a = 3;
+  printf("El cubo de la variable a es : %i\n", CUBO(a));
+  return 0;
+}
