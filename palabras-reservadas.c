801521aa8c96865b6d848925911112c5b94e4d41
javier73 <ingenierodesoftware73@gmail.com>
2021-06-15 09:54:43 -0500

    practice with reserved words

---
+++ palabras-reservadas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+* palabras reservadas
+*/
+int main(){
+  int a = 80;
+  float f = 45.3;
+  float suma = (float)a + f;
+  printf("el resultado es: %.2f", suma);
+  return 0;
+}
